432c81cf7a5467e6d5d52d58d5f851fc6e3b19b1
barthelemy pousset <barthelemypousset@gmail.com>
2026-01-29 15:53:45 +0100

    moved surprise into scotch component

--- app/components/ScotchedPhoto.tsx
+++ app/components/ScotchedPhoto.tsx
@@ -23,28 +23,40 @@ const ScotchedPhoto: React.FC<ScotchedPhotoProps> = ({ image, containerClassName
   const handleScotchClick = (scotchId: number) => {
     // Avoid re-adding if already clicked, and do nothing if it's already falling
     if (isFalling || clickedScotches.has(scotchId)) return;
-    setClickedScotches(prev => new Set(prev).add(scotchId));
+    setClickedScotches((prev) => new Set(prev).add(scotchId));
   };
 
-  const fallAnimationClasses = isFalling
-    ? "transition-all duration-[2000ms] ease-in transform translate-y-[200vh] rotate-[30deg]"
-    : "";
+  const fallAnimationClasses =
+    isFalling ? "transition-all duration-[1500ms] ease-in transform translate-y-[100vh] rotate-[30deg]" : "";
 
   return (
-    <div className={`${containerClassName || ""} absolute ${fallAnimationClasses}`}>
-      <Image {...image} alt="Phicture of the webmaster" className="w-full border-8 border-white" />
-      <div
-        onClick={() => handleScotchClick(1)}
-        className={`absolute bottom-0 -left-4 w-16 h-6 bg-white/50 transform rotate-45 backdrop-blur-sm cursor-pointer transition-opacity duration-300 ${
-          clickedScotches.has(1) ? "opacity-0" : "opacity-100"
-        }`}
-      />
-      <div
-        onClick={() => handleScotchClick(2)}
-        className={`absolute top-0 -right-4 w-16 h-6 bg-white/50 transform rotate-45 backdrop-blur-sm cursor-pointer transition-opacity duration-300 ${
-          clickedScotches.has(2) ? "opacity-0" : "opacity-100"
-        }`}
-      />
+    <div className={`${containerClassName || ""} absolute`}>
+      <a
+        href="https://shattereddisk.github.io/rickroll/rickroll.mp4"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="absolute w-1/2 translate-y-1/2 translate-x-1/2 rotate-12 flex items-center justify-center"
+        aria-label="A surprise link">
+        <span className="text-[10vh]" role="img" aria-hidden="true">
+          🤡
+        </span>
+      </a>
+
+      <div className={`${fallAnimationClasses} z-20`}>
+        <Image {...image} alt="Picture of the webmaster" className="relative w-full border-8 bg-white border-white" />
+        <div
+          onClick={() => handleScotchClick(1)}
+          className={`absolute bottom-0 -left-4 w-16 h-6 bg-white/50 transform rotate-45 backdrop-blur-sm cursor-pointer transition-opacity duration-300 ${
+            clickedScotches.has(1) ? "opacity-0" : "opacity-100"
+          }`}
+        />
+        <div
+          onClick={() => handleScotchClick(2)}
+          className={`absolute top-0 -right-4 w-16 h-6 bg-white/50 transform rotate-45 backdrop-blur-sm cursor-pointer transition-opacity duration-300 ${
+            clickedScotches.has(2) ? "opacity-0" : "opacity-100"
+          }`}
+        />
+      </div>
     </div>
   );
 };
